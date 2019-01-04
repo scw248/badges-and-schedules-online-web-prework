816f51c40843790bbd83afe11298665203ee714c
Stephen Wright <scw248@gmail.com>
2019-01-04 00:57:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def batch_badge_creator(array_of_names)
 
 def assign_rooms(attendees)
  arr = [] 
- room_number = 1 + rand(7).uniq
+ room_number = 1 + rand(7)
  attendees.each do |name|
    arr << "Hello, #{name}! You'll be assigned to room #{room_number}!"
  end
