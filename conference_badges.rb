d2628e0c97989b2e85471b56d76a380b2fdd37b9
Stephen Wright <scw248@gmail.com>
2019-01-04 01:24:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,12 +25,10 @@ def assign_rooms(attendees)
  
  def printer(attendees)
    loop do
-   arr = [] 
-   room_assignment = [1, 2, 3, 4, 5, 6, 7]
- attendees.each do |name|
-   arr << "Hello, my name is #{name}."
-   arr << "Hello, #{name}! You'll be assigned to room #{room_assignment[0]}!"
-   room_assignment.shift
-   puts arr
- end
+   puts batch_badge_creator(attendees[0])
+   puts assign_rooms(attendees[0])
+   attendees.shift
+   if attendees.length == 0
+     break
+   end
  end
